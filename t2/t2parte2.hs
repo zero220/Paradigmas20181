ae324f1a8934c954f88ce0d2b46388173f2deca1
João Vitor Azevedo da Costa <joaovitor.azevedo.522@gmail.com>
2018-04-02 18:38:02 -0300

    T2 Parte 2

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -6,3 +6,14 @@ isBin (x:xs)= if (x=='0' || x=='1') then isBin(xs) else False
 
 isBin' :: String -> Bool
 isBin' str = length(filter (`notElem` "01") str) ==0 && str/=""
+
+auxBin2Dec :: Int -> Int -> Int
+auxBin2Dec num expn = num ^ expn
+
+bin2dec :: [Int] -> Int
+bin2dec [] =undefined
+bin2dec [1] = 1
+bin2dec [0] = 0
+bin2dec (x:xs)
+     |x==1 || x==0        =auxBin2Dec (2*x) (length(xs)) + bin2dec(xs)
+     |otherwise           =undefined
